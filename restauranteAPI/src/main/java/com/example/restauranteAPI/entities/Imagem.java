695f0b1c57be3dec0557efe9794b17e2446d1599
Feliphe Xavier <feliphexavier2604@gmail.com>
2025-05-12 17:20:28 -0300

    definicao das classes categoria, imagem e produto

--- restauranteAPI/src/main/java/com/example/restauranteAPI/entities/Imagem.java
+++ restauranteAPI/src/main/java/com/example/restauranteAPI/entities/Imagem.java
@@ -1,4 +1,30 @@
 package com.example.restauranteAPI.entities;
 
-public class Image {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Imagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nomeArquivo;
+    private String tipoArquivo;
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+    @OneToOne
+    @JoinColumn(name = "id_produto")
+    private Produto produto;
+
+
 }
--- restauranteAPI/src/main/java/com/example/restauranteAPI/entities/Produto.java
+++ restauranteAPI/src/main/java/com/example/restauranteAPI/entities/Produto.java
@@ -1,4 +1,28 @@
 package com.example.restauranteAPI.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="id_categoria")
+    private Categoria categoria;
+    @OneToOne(mappedBy = "produto", cascade = CascadeType.ALL)
+    private Imagem imagem;
 }
